9339c1a3b727ad2ac6ff283a53b42379d52a44b6
fabibrasil <fabiana.sousa@ufu.br>
2023-08-22 20:09:36 -0300

    ex1

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Fabiana Brasil - 12211bcc015");
+    }
+}
